0bb2cd8972dca37c0c87e4f900ceaccc71c07e44
PanBaron <artur.mykowski@gmail.com>
2022-11-02 12:45:26 +0100

    Add project files.

---
+++ DataCaptureService/Program.cs
@@ -0,0 +1,16 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataCaptureService
+{
+    internal class Program
+    {
+
+    }
+}
---
+++ RabbitMQConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiverService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
