4763ddae076df01cb532d24c08fc8877e5d3398f
PanBaron <artur.mykowski@gmail.com>
2022-11-02 12:47:39 +0100

    Development of task

--- DataCaptureService/Program.cs
+++ DataCaptureService/Program.cs
@@ -11,6 +11,37 @@ namespace DataCaptureService
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            var path = @"D:\PdfFolder";
 
+            var watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            watcher.Filter = "*.pdf";
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+
+            Console.ReadLine();
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "NewQueue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                byte[] bytes = System.IO.File.ReadAllBytes(e.FullPath);
+                channel.BasicPublish(exchange: "",
+                    routingKey: "NewQueue",
+                    basicProperties: null, bytes);
+            }
+        }
     }
 }
